90dc3d4051ff4d6ef9778cc446b548e99bd78318
John HU <hszqqq12@gmail.com>
2024-12-16 20:29:06 +0000

    Add a inspirational implementation for day 16

--- day-16/day-16.py
+++ day-16/day-16.py
@@ -96,3 +96,43 @@ while q:
 
 print(best)
 print(len(set(bestpath)))
+
+# good inspo found on reddit:
+# https://www.reddit.com/r/adventofcode/comments/1hfboft/comment/m2bcfmq/
+# https://topaz.github.io/paste/#XQAAAQACAwAAAAAAAAAzHIoib6pXbueH4X9F244lVRDcOZab5q1+VXY/ex42qR7D+RJIsq5/YCi6YHJak4tBC2I+oV44GGsk3yILWGnOyXBRhMTGj1iPohCpnraI7cOVIQMgn6rgSzIs8ivVrbZy5UZbCRK5ynh8s/ewf4aWP/ziHGlRvx+evmR5c+dJCcg4aXptpJ4wX011RpSp9CrrqxfG6AHLLvuWe3uAsiz3BMUMAxoduis7+KevEU1N9ZpXOg+AllGx9HrV+/PBI/U7z4HjfpO5j1HdlhNfEp3Qlhydf4IQ/6nQvX39jWc8fG7LXD/YiMj89zdaG/93MmfrJX7dGkxO5W/kKqZqO/tNiX4gHNh8VgWnSZKMZFStlzAbhBBjgAAhbcxSnU9IFqa2T3RwFdPZVnL0q/xI7M4vq/vbdJo4mVGhuuAZwcCRCk7mwXcJxfVxZDgokb7Njdezsi/RwHybpdPs9vYGGixqU7aEfM+AJGtAU9ALoQN+8xKCaeOkuee7lSEd2Qx6qmuA9mcR7JX34lwmrJd0f8PrWWIjtAYYZP787Tg8B22iFBM02EmP5mt5A+hB7aSrldDMh2mb91VXFf/ztA22
+#
+# this solution applies the following which make the code more clean and efficient
+# - compiles the maze into one-dimension
+# - uses complex numbers to represent the direction
+# - uses heapq to manage the queue in Dijkstra
+
+from collections import defaultdict
+from heapq import heappop, heappush
+
+grid = {i+j*1j: c for i,r in enumerate(open('input.txt'))
+                  for j,c in enumerate(r) if c != '#'}
+
+start, = (p for p in grid if grid[p] in 'S')
+
+seen = []
+best = 1e9
+dist = defaultdict(lambda: 1e9)
+todo = [(0, t:=0, start, 1j, [start])]
+
+while todo:
+    score, _, pos, dir, path = heappop(todo)
+
+    if score > dist[pos, dir]: continue
+    else: dist[pos, dir] = score
+
+    if grid[pos] == 'E' and score <= best:
+        seen += path
+        best = score
+
+    for rot, pts in (1, 1), (1j, 1001), (-1j, 1001):
+        new = pos + dir*rot
+        if new in grid:
+            heappush(todo, (score + pts, t := t+1,
+                new, dir*rot, path + [new]))
+
+print(best, len(set(seen)))
